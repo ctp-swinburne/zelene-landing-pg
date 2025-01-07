1e45f4787827f12bdfafed7d06d78863ec34885b
QiuTee <nqt8977@gmail.com>
2025-01-07 15:41:10 +0700

    fix error

--- src/app/admin/(query-management)/feedback/_components/FeedbackDetailDrawer.tsx
+++ src/app/admin/(query-management)/feedback/_components/FeedbackDetailDrawer.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React from "react";
-import { Drawer, Select, Space, Tag, Rate } from "antd";
+import { Drawer, Descriptions, Button, Select, Space, Tag, Rate } from "antd";
 
 const QueryStatus = {
   NEW: "NEW",
